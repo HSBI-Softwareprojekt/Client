175b62688928255e42ef03ba9771a11b6adfda14
Alexey Khokhlov <alexey.khokhlov@fh-bielefeld.de>
2024-05-14 16:50:46 +0200

    Login and register input fields can be written. Input fileds are accessible in C#

--- Puddle Partners/Assets/login.cs
+++ Puddle Partners/Assets/login.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class login : MonoBehaviour
 {
+
+    public TMP_InputField username;
+    public TMP_InputField password;
  
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void RequestLogin()
+    {
+        print(username.text);
+        print(password.text);
     }
 
     public void QuitGame()
--- Puddle Partners/Assets/register.cs
+++ Puddle Partners/Assets/register.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class register : MonoBehaviour
 {
-    
+
+    public TMP_InputField mail;
+    public TMP_InputField username;
+    public TMP_InputField password;
+    public TMP_InputField password_wd;
+
     public void RegisterUser()
     {
 
